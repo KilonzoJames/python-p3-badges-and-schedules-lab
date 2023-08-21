a1ffb0ba03e8ee195b8c7a4ec1a55bc4f5b914e2
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-21 19:57:56 +0300

    How to Iterate a Python List With Index?

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -20,3 +20,24 @@ def printer(names):
         print(badge_message)
     for room_assignment in room_assignments:
         print(room_assignment)
+
+
+# countries = ['US', 'GB', 'CA', 'DE']
+# for index, item in enumerate(countries):
+#     print(index, item)
+
+# countries = ['US', 'GB', 'CA', 'DE']
+# index = 0
+# while index < len(countries):
+#     print(index, countries[index])
+#     index += 1
+
+# countries = ['US', 'GB', 'CA', 'DE']
+# for index in range(len(countries)):
+#     print(index, countries[index])
+
+# countries = ['US', 'GB', 'CA', 'DE']
+# index = 0
+# for item in countries:
+#     print(index, item)
+#     index += 1
